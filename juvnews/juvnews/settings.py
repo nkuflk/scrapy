415825920da42705e4e8ddbc429c94399de1ee77
yuntao.hyt <heyuntao789789@163.com>
2015-06-15 00:05:20 +0800

    first

---
+++ juvnews/juvnews/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for juvnews project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'juvnews'
+
+SPIDER_MODULES = ['juvnews.spiders']
+NEWSPIDER_MODULE = 'juvnews.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'juvnews (+http://www.yourdomain.com)'
---
+++ juvnews/juvnews/spiders/juvnews_spider.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+#########################################
+# File Name: juvnews_spider.py
+# Author: F.L.K
+# Mail: nkuflk@gmail.com
+# Created Time: 2015-06-14 23:42:45
+#########################################
+
+import scrapy
+from juvnews.items import JuvnewsItem
+
+class JuvnewsSpider(scrapy.spider.Spider):
+	name = 'juvnews'
+	allowed_domains = ['163.com']
+	start_urls = [
+		'http://sports.163.com/special/00051NSJ/morejuv.html'
+	]
+
+	def parse(self, response):
+		for sel in response.xpath('//div[@class="col2"]/ul/li'):
+			item = JuvnewsItem()
+			item['title'] = sel.xpath('span/a/text()').extract()
+			item['url'] = sel.xpath('span/a/@href').extract()
+			item['date'] = sel.xpath('span[@class="postTime"]/text()').extract()
+			yield item
